df2cef453b333b4d6e081a688bb60c567e3a3f37
Yoga Hapriana <thenelse.v2@gmail.com>
2024-04-23 14:52:05 +0700

    Writing markup with JSX

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import Profile from './Profile.js';
-
-export default function Gallery() {
+export default function TodoList() {
   return (
-    <section>
-      <h1>Amazing scientists</h1>
-      <Profile />
-      <Profile />
-      <Profile />
-    </section>
+    <>
+      <h1>Hedy Lamarr's Todos</h1>
+      <img
+        src="https://i.imgur.com/yXOvdOSs.jpg"
+        alt="Hedy Lamarr"
+        className="photo"
+      />
+      <ul>
+        <li>Invent new traffic lights</li>
+        <li>Rehearse a movie scene</li>
+        <li>Improve spectrum technology</li>
+      </ul>
+    </>
   );
 }
