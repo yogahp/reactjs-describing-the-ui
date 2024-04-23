a2f3d97d13558ea78a6179f3409bb09095201317
Yoga Hapriana <thenelse.v2@gmail.com>
2024-04-23 14:58:42 +0700

    Feature/describing the UI (#1)
    * Your first component
    
    * Importing and exporting components
    
    * Writing markup with JSX
    
    * JavaScript in JSX with curly braces
    
    * Passing props to a component
    
    * Conditional rendering
    
    * Rendering lists

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { people } from './data.js';
+import { getImageUrl } from './utils.js';
 
-function App() {
+export default function List() {
+  const listItems = people.map(person =>
+    <li key={person.id}>
+      <img
+        src={getImageUrl(person)}
+        alt={person.name}
+      />
+      <p>
+        <b>{person.name}:</b>
+        {' ' + person.profession + ' '}
+        known for {person.accomplishment}
+      </p>
+    </li>
+  );
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <article>
+      <h1>Scientists</h1>
+      <ul>{listItems}</ul>
+    </article>
   );
 }
-
-export default App;
