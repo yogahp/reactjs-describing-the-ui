905f9ac0268054bd08e96d351e6bcae53ed631cf
Yoga Hapriana <thenelse.v2@gmail.com>
2024-04-23 14:56:01 +0700

    Conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,36 +1,29 @@
-import { getImageUrl } from './utils.js'
-
-export default function Profile() {
+function Item({ name, isPacked }) {
   return (
-    <Card>
-      <Avatar
-        size={100}
-        person={{
-          name: 'Katsuko Saruhashi',
-          imageId: 'YfeOqp2'
-        }}
-      />
-    </Card>
+    <li className="item">
+      {name} {isPacked && '✔'}
+    </li>
   );
 }
 
-function Avatar({ person, size }) {
+export default function PackingList() {
   return (
-    <img
-      className="avatar"
-      src={getImageUrl(person)}
-      alt={person.name}
-      width={size}
-      height={size}
-    />
+    <section>
+      <h1>Sally Ride's Packing List</h1>
+      <ul>
+        <Item
+          isPacked={true}
+          name="Space suit"
+        />
+        <Item
+          isPacked={true}
+          name="Helmet with a golden leaf"
+        />
+        <Item
+          isPacked={false}
+          name="Photo of Tam"
+        />
+      </ul>
+    </section>
   );
 }
-
-function Card({ children }) {
-  return (
-    <div className="card">
-      {children}
-    </div>
-  );
-}
-
